d8f7cc2284308f2292b92fc263d9c77b454d12ae
jhosef1234 <jhoseqh123@gmail.com>
2022-11-25 17:43:33 -0500

    iterator

---
+++ Prote/src/main/java/pe/edu/upeu/prote/Prote.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package pe.edu.upeu.prote;
+
+import pe.edu.upeu.prote.iterator.Iterator;
+import pe.edu.upeu.prote.iterator.NameRepository;
+
+/**
+ *
+ * @author HP
+ */
+public class Prote {
+
+    public static void main(String[] args) {
+        NameRepository namesRepository = new NameRepository();
+
+        for (Iterator iter = namesRepository.getIterator(); iter.hasNext();) {
+            String name = (String) iter.next();
+            System.out.println("Name : " + name);
+        }
+    }
+}
---
+++ Prote/src/main/java/pe/edu/upeu/prote/iterator/Iterator.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package pe.edu.upeu.prote.iterator;
+
+public interface Iterator {
+   public boolean hasNext();
+   public Object next();
+}
